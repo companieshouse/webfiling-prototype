3d96f239d6dd2d6e5691d3926f5dc6d1cd0efa2e
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-08-15 08:07:09 +0100

    Updated routing

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,18 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/choose-design', function(request, response) {
+    response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-status")
+})
+
+router.post('/exempt-status', function(request, response) {
+
+    var design = request.session.data['design']
+    if (design == "autocomplete"){
+        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason-autocomplete")
+    } else {
+        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason")
+    }
+})
+
