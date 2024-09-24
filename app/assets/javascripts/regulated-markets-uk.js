bd797d0dd86d48493c689a3b40c76aad73539372
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-09-24 16:02:58 +0100

    Updated journey

---
+++ app/assets/javascripts/regulated-markets-uk.js
@@ -0,0 +1,16 @@
+var countries = [
+  'United Kingdom (UK) - The London Metal Exchange',
+  'United Kingdom (UK) - ICE Futures Europe',
+  'United Kingdom (UK) - London Stock Exchange',
+  'United Kingdom (UK) - Aquis Stock Exchange Limited',
+  'United Kingdom (UK) - Cboe Europe Equities Regulated Market'
+var element = document.querySelector('#market')
+var elementTwo = document.querySelector('#marketTwo')
+var elementThree = document.querySelector('#marketThree')
+var id = 'autocomplete-default'
+accessibleAutocomplete({element: element, id: id, source: countries, name: "marketAppoint"})
+accessibleAutocomplete({element: elementTwo, id: id, source: countries, name: "marketAppoint"})
+accessibleAutocomplete({element: elementThree, id: id, source: countries, name: "marketAppoint"})
+
+
+
--- app/routes.js
+++ app/routes.js
@@ -12,13 +12,17 @@ router.post('/choose-design', function(request, response) {
     response.redirect("/conf-statement-enhancement/psc-exemptions/trading-on-a-market")
 })
 
+router.post('/exempt-reason-radios', function(request, response) {
+    response.redirect("/conf-statement-enhancement/psc-exemptions/choose-market-autocomplete")
+})
+
 router.post('/exempt-status', function(request, response) {
 
     var design = request.session.data['design']
     if (design == "nested"){
         response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason")
     } else {
-        response.redirect("/conf-statement-enhancement/psc-exemptions/choose-market-autocomplete")
+        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason-radios")
     }
 })
 
