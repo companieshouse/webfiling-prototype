2931c58ca431b9d4ecbc343366b4959b671a04d8
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-10-08 08:31:08 +0100

    Updated routes

--- app/routes.js
+++ app/routes.js
@@ -8,9 +8,28 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+
+
+
 router.post('/choose-design', function(request, response) {
     response.redirect("/conf-statement-enhancement/psc-exemptions/trading-on-a-market")
 })
+
+
+
+
+router.post('/trading-on-a-market', function(request, response) {
+
+    var statement = request.session.data['statement']
+    if (statement == "no"){
+        response.redirect("/conf-statement-enhancement/psc-exemptions/confirmation-statement-accordion")
+    } else {
+        response.redirect("/conf-statement-enhancement/psc-exemptions/dtr5")
+    }
+})
+
+
+
 
 router.post('/exempt-reason-radios', function(request, response) {
     response.redirect("/conf-statement-enhancement/psc-exemptions/choose-market-autocomplete")
@@ -18,11 +37,23 @@ router.post('/exempt-reason-radios', function(request, response) {
 
 router.post('/exempt-status', function(request, response) {
 
-    var design = request.session.data['design']
-    if (design == "nested"){
-        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason")
+    var exempt = request.session.data['exempt']
+    if (exempt == "no"){
+        response.redirect("/conf-statement-enhancement/psc-exemptions/confirmation-statement-accordion")
     } else {
-        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason-radios")
+        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason-autocomplete")
     }
 })
+
+
+router.post('/exemptions-check-answers', function(request, response) {
+
+    var addMarket = request.session.data['addMarket']
+    if (addMarket == "yes"){
+        response.redirect("/conf-statement-enhancement/psc-exemptions/exempt-reason-autocomplete")
+    } else {
+        response.redirect("/conf-statement-enhancement/psc-exemptions/confirmation-statement-accordion")
+    }
+})
+
 
