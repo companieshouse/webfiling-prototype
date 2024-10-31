182551a7d36c0a6b014631a5bae92598e4ee0553
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-10-31 12:11:30 +0000

    Reinstated autocomplete for UK markets

--- app/routes.js
+++ app/routes.js
@@ -31,19 +31,12 @@ router.post('/trading-on-a-market', function(request, response) {
 
 
 
-router.post('/exempt-reason-radios', function(request, response) {
+
+
+router.post('/exempt-reason-autocomplete', function(request, response) {
     response.redirect("/conf-statement-enhancement/psc-exemptions/choose-market-autocomplete")
 })
 
-router.post('/choose-market-autocomplete', function(request, response) {
-
-    var statement = request.session.data['statement']
-    if (statement == "uk"){
-        response.redirect("/conf-statement-enhancement/psc-exemptions/uk-regulated-market")
-    } else {
-        response.redirect("/conf-statement-enhancement/psc-exemptions/choose-market-autocomplete")
-    }
-})
 
 router.post('/exempt-status', function(request, response) {
 
